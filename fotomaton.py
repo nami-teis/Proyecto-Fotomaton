8ee33d00b6750eb10215373477748d92ae6a801f
nami-teis <148221435+nami-teis@users.noreply.github.com>
2024-03-25 18:24:14 +0100

    Clase Fotomaton

--- fotomaton.py
+++ fotomaton.py
@@ -4,31 +4,37 @@ import time
 from Camara import Camara
 from Imagen import Imagen
 
-dimensiones = (2560, 1080)#ancho alto
-cam = Camara(dimensiones)
-img = Imagen("foto.jpg")
+class Fotomaton: 
+    dimensiones : tuple[int, int]
+    cam : Camara
+    img : Imagen
 
-foto = False
-while not foto:
-    # Se comprueba que se obtiene imagen
-    ret, frame = cam.leer()
+    def __init__(self, dimensiones: tuple[int, int]):
+        self.cam = Camara(dimensiones)
+        self.img = Imagen("foto.jpg")
 
-    if ret == False:
-        raise Exception ("No se ha podido obtener ninguna imagen de la camara")
+    def ejecutar(self): 
+        foto = False
+        while not foto:
+            # Se comprueba que se obtiene imagen
+            ret, frame = self.cam.leer()
 
-    img.mostrar(frame)
+            if ret == False:
+                raise Exception ("No se ha podido obtener ninguna imagen de la camara")
 
-    # Si se presiona la tecla de esc se guardará la foto, esto se deberá reemplazar con pygame
-    if cv2.waitKey(1) == 27:
-        img.guardar(frame)
-        time.sleep(5)
-        respuesta = input("¿Te gusta la imagen? s|n: ")
-        if respuesta.lower() == "s":
-            # se tendría que poner el comando de la impresora
-            # os.startfile(nombre_archivo, "print") o hacer un script :/
-            foto = True
-        else:
-            img.borrar()
+            self.img.mostrar(frame)
 
-cam.finalizar()
-img.borrar()
+            # Si se presiona la tecla de esc se guardará la foto, esto se deberá reemplazar con pygame
+            if cv2.waitKey(1) == 27:
+                self.img.guardar(frame)
+                time.sleep(5)
+                respuesta = input("¿Te gusta la imagen? s|n: ")
+                if respuesta.lower() == "s":
+                    # se tendría que poner el comando de la impresora
+                    # os.startfile(nombre_archivo, "print") o hacer un script :/
+                    foto = True
+                else:
+                    self.img.borrar()
+
+        self.cam.finalizar()
+        self.img.borrar()
